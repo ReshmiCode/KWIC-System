5c19e5ef603faf59bc5dee670dd984557d54e1b8
amrutha-bhargavi <amruthabhargavi1995@gmail.com>
2020-09-17 17:54:12 -0500

    update

---
+++ src/CircularShift.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class CircularShift {
+
+    List<String> lines;
+    List<String> circularList;
+
+    public CircularShift(List<String> lines) {
+        this.lines = lines;
+    }
+
+}
